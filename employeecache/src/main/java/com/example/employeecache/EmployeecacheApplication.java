59aa402ca3e35602bd19cc6b03c07768876b2e08
Logesh.R <Logesh.R@eNoah.CHN>
2022-08-17 17:25:23 +0530

    initialcommit

---
+++ employeecache/src/main/java/com/example/employeecache/EmployeecacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeecacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeecacheApplication.class, args);
+	}
+
+}
---
+++ employeecache/src/test/java/com/example/employeecache/EmployeecacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeecacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
